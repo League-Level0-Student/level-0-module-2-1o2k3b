aac041ea0bff24e86735c988bdb4efc015fa825a
root <root@46a4140a594e>
2022-03-08 02:29:31 +0000

    yay!5069034237985467

--- src/_02_methods/_1_animal_farm/AnimalFarm.java
+++ src/_02_methods/_1_animal_farm/AnimalFarm.java
@@ -1,3 +1,4 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2020
 //    Level 0
 
@@ -10,28 +11,43 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class AnimalFarm {
-	
-	/***********  SOUND ***************
-	 * Some computers are unable to play sounds. 
-	 * If you cannot play sound on this computer, set canPlaySounds to false.
-	 * If you are not sure, ask your teacher 
-	 * *****************/
+
+	/***********
+	 * SOUND *************** Some computers are unable to play sounds. If you cannot
+	 * play sound on this computer, set canPlaySounds to false. If you are not sure,
+	 * ask your teacher
+	 *****************/
 	boolean canPlaySounds = false;
 
-	
 	public static void main(String[] args) {
 		new AnimalFarm().animals();
 	}
-	
+
 	void animals() {
 
-		/* 1. Ask the user which animal they want, then see and hear 
-		 *    the animal they chose using one of the methods below.
-		*/	
-				for(int i = 0; i<6; i++) { 
-					JOptionPane.showInputDialog("which farm animal do you want");		 
-				}
-			
+		/*
+		 * 1. Ask the user which animal they want, then see and hear the animal they
+		 * chose using one of the methods below.
+		 */
+		for (int i = 0; i < 7; i++) {
+			String answer = JOptionPane.showInputDialog("which farm animal do you want");
+			if (answer.equals("cow")) {
+				moo();
+			}
+			if (answer.equals("duck")) {
+				quack();
+			}
+			if (answer.equals("dog")) {
+				woof();
+			}
+			if (answer.equals("cat")) {
+				meow();
+			}
+			if (answer.equals("llama")) {
+				llamaScream();
+			}
+		}
+
 		/* 2. Make it so that the user can keep entering new animals. */
 
 	}
@@ -50,20 +66,16 @@ public class AnimalFarm {
 		playNoise(woofFile);
 		showImage(dogIcon);
 	}
-	
+
 	void meow() {
 		playNoise(meowFile);
 		showImage(catIcon);
 	}
-	
+
 	void llamaScream() {
 		playNoise(llamaFile);
 		showImage(llamaIcon);
 	}
-	
-
-
-	
 
 	/* Ignore this stuff */
 	String path = "src/_02_methods/_1_animal_farm/";
@@ -77,41 +89,41 @@ public class AnimalFarm {
 	ImageIcon dogIcon;
 	ImageIcon duckIcon;
 	ImageIcon cowIcon;
-	
-	private void showImage (ImageIcon icon) {
-			JOptionPane.showMessageDialog(null, "", "You chose", 0, icon);
+
+	private void showImage(ImageIcon icon) {
+		JOptionPane.showMessageDialog(null, "", "You chose", 0, icon);
 	}
-	
+
 	private void playNoise(String soundFile) {
-		if (canPlaySounds) {	
-			File sound = new File(path+soundFile);
+		if (canPlaySounds) {
+			File sound = new File(path + soundFile);
 			if (sound.exists()) {
 				new Thread(() -> {
-				try {
-					Clip clip = AudioSystem.getClip();
-					clip.open(AudioSystem.getAudioInputStream(sound));
-					clip.start();
-					Thread.sleep(clip.getMicrosecondLength()/1000);
-				}
-				catch (Exception e) {
-					System.out.println("Could not play this sound");
-				}}).start();
-	 		}
-			else {
+					try {
+						Clip clip = AudioSystem.getClip();
+						clip.open(AudioSystem.getAudioInputStream(sound));
+						clip.start();
+						Thread.sleep(clip.getMicrosecondLength() / 1000);
+					} catch (Exception e) {
+						System.out.println("Could not play this sound");
+					}
+				}).start();
+			} else {
 				System.out.println("File does not exist");
 			}
 		}
 	}
+
 	public AnimalFarm() {
 		try {
-			llamaIcon = new ImageIcon(path+"llama.jpg");
-			cowIcon = new ImageIcon(path+"cow.jpg");
-			dogIcon = new ImageIcon(path+"dog.jpg");
-			catIcon = new ImageIcon(path+"cat.jpg");
-			duckIcon = new ImageIcon(path+"duck.jpg");
+			llamaIcon = new ImageIcon(path + "llama.jpg");
+			cowIcon = new ImageIcon(path + "cow.jpg");
+			dogIcon = new ImageIcon(path + "dog.jpg");
+			catIcon = new ImageIcon(path + "cat.jpg");
+			duckIcon = new ImageIcon(path + "duck.jpg");
 
 		} catch (Exception e) {
-					
+
 		}
 	}
 }
